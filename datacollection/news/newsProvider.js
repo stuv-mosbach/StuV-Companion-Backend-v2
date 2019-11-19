272fc45d3944b4a187114609eeb2d7dc26c8e1b2
Supporterino <lars@roth-kl.de>
2019-11-19 14:11:34 +0100

    added news update

--- datacollection/news/newsProvider.js
+++ datacollection/news/newsProvider.js
@@ -9,14 +9,27 @@ var newsUrl = 'https://stuv-mosbach.de/feed/';
 
 exports.run = () => {
     return new Promise((resolve, reject) => {
-
+        loadFeed(newsUrl, resolve, reject);
     })
 }
 
 const loadFeed = async (url, resolve, reject) => {
     let newsData = await Reader.parseURL(newsUrl);
+    newsData.items.forEach(e => {
+        updateDBEntrys(e, reject)
+    });
+    resolve();
 }
 
-const updateDBEntrys = (data, resolve, reject) => {
-    
+const updateDBEntrys = (element, reject) => {
+    const data = { creator: element["creator"], title: element["title"], link: element["link"], pubDate: element["pubDate"], 'content:encoded': element["content:encoded"], 'dc:creator': element["dc:creator"], content: element["content"], contentSnippet: element["contentSnippet"], guid: element["guid"], isoDate: element["isoDate"] };
+    const options = { upsert: true, new: true, useFindAndModify: false };
+    const query = { isoDate: element["isoDate"] };
+    news.findOneAndUpdate(query, data, options)
+        .then((doc) => {
+
+        })
+        .catch((err) => {
+            reject(err);
+        });
 }
